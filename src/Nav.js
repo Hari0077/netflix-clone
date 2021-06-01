13a150f6c3114a1d680b16d45ab153f818b4123b
Hariharan S <hari61000@gmail.com>
2021-06-01 22:09:52 +0530

    initial commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router';
 import "./Nav.css";
 function Nav() {
 
     const [show, handleShow] = useState(false);
+    const history = useHistory();
+    const _fnEmptyFunctionPointer = () => { };
 
     // const transitionNavbar = () => {
     //     if (window.screenY > 100) {
@@ -23,7 +26,7 @@ function Nav() {
             } else handleShow(false);
         });
         return () => {
-            window.removeEventListener("scroll");
+            window.removeEventListener("scroll", _fnEmptyFunctionPointer);
         };
     }, []);
 
@@ -33,11 +36,13 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
             {/* < div className="nav__contents" > */}
             <img
+                onClick={() => history.push("/")}
                 className="nav__logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png"
                 alt=""
             />
             <img
+                onClick={() => history.push("/profile")}
                 className="nav__avatar"
                 src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
                 alt=""
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCqSvJTgJtSv_HV3wjIMQq8cUaUU-ZPQ8w",
     authDomain: "netflix-clone-c3100.firebaseapp.com",
@@ -6,3 +9,11 @@ const firebaseConfig = {
     messagingSenderId: "924280379249",
     appId: "1:924280379249:web:daadae703aad06423048d0"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export { auth };
+export default db;
